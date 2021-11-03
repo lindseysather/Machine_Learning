5a011d844cff4d8f4b952d101e676e754d11d41f
Lindsey <lindsey_sather1@baylor.edu>
2021-11-03 17:28:39 -0500

    class 2

--- ml1.py
+++ ml1.py
@@ -7,7 +7,7 @@ from sklearn.datasets import load_digits
 #   3. digits.images
 
 digits = load_digits()
-
+'''
 #print(digits.DESCR)
 
 #tells you pixel intensity of image (64 pixels of an 8x8 image of a number):
@@ -47,5 +47,70 @@ for item in zip(axes.ravel(), digits.images, digits.target):
 
 plt.tight_layout()
 #plt.show()
+'''
+
+from sklearn.model_selection import train_test_split
+
+#split data into training and testing (75% into training)
+#normally don't need random_state (just for testing in class)
+data_train, data_test, target_train, target_test = train_test_split(
+    digits.data, digits.target, random_state=11
+)
+
+#2D
+print(data_train.shape)
+print(data_test.shape)
+
+#1D
+print(target_train.shape)
+print(target_test.shape)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier()
+
+#notice x is uppercase, y is lowercase
+#fit is the algorithm that does all the machine learning 
+    #needs the data and the target
+    #training the model
+    #tells what data matches up with each target
+knn.fit(X=data_train, y=target_train)
+
+#predict will tell you the answer  (an array of all the targets)
+predicted = knn.predict(X=data_test)
+expected = target_test
+#predicted and expected should match up - proves that the algorithm has learned and is working correctly
+
+print(predicted[:20])
+print(expected[:20])
+
+#Tells you how well it does - how much the predicted matches up with expected (97.78%)
+print(format(knn.score(data_test, target_test), ".2%"))
+
+#prints wrong predictions - gets 10 wrong of the 450
+wrong = [(p,e) for (p,e) in zip(predicted, expected) if p != e]
+print(wrong)
+
+
+#CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix
+
+confusion = confusion_matrix(y_true=expected, y_pred=predicted)
+print(confusion)
+
+
+#Heat Map
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt2
+
+confusion_df = pd.DataFrame(confusion, index=range(10), columns=range(10))
+
+figure = plt2.figure(figsize=(7,6))
+axes = sns.heatmap(confusion_df, annot=True, cmap=plt2.cm.nipy_spectral_r)
+
+plt2.show()
+
 
 
