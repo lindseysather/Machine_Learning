a9aacdc94d80d4aaaa7e1af99772d4684374dfc4
Lindsey <lindsey_sather1@baylor.edu>
2021-11-10 16:42:59 -0600

    what's on the quiz

---
+++ quiz.py
@@ -0,0 +1,15 @@
+'''
+Quiz:
+
+Won't need to split (two different files - predicted and expected)
+    No train, test split
+
+First file is the training file (Game ratings)
+Second is the test file (Test)
+
+Feed model with test data that correctly predicts the rating 
+
+Produce CSV file that tells rating (in words (Teen)) for every game name 
+
+
+'''
